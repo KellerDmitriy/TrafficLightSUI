46fee0a0fa80ec95a36c284b384d100b071e1d15
DmKeller <dmit.keller@gmail.com>
2023-06-09 18:53:11 +0300

    completed task 4.1

---
+++ TrafficLight/CircleView.swift
@@ -0,0 +1,46 @@
+//
+//  CircleView.swift
+//  TrafficLight
+//
+//  Created by Келлер Дмитрий on 09.06.2023.
+//
+
+import SwiftUI
+
+enum Condition {
+    case lightIsOn
+    case lightIsOff
+    
+    var opacity: Double {
+        switch self {
+        case .lightIsOn:
+            return 1.0
+        case .lightIsOff:
+            return 0.3
+        }
+    }
+}
+
+struct CircleView: View {
+    let color: Color
+    let condition: Condition
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(color)
+                .frame(width: 130, height: 130)
+                .overlay(Circle()
+                    .stroke(Color.white, lineWidth: 1))
+                .shadow(color: color, radius: 40)
+                .opacity(condition.opacity)
+        }
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView(color: .green, condition: .lightIsOff )
+        
+    }
+}
--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -8,19 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentText = "START"
+    @State private var currentLight = Color.red
+    
+    @State private var redLightCondition: Condition = .lightIsOff
+    @State private var yellowLightCondition: Condition = .lightIsOff
+    @State private var greenLightCondition: Condition = .lightIsOff
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20){
+                CircleView(color: .red, condition: redLightCondition)
+                CircleView(color: .yellow, condition: yellowLightCondition)
+                CircleView(color: .green, condition: greenLightCondition)
+                
+                    .padding(.bottom, 50)
+                Button(action: {
+                    switch currentLight {
+                    case .red:
+                        greenLightCondition = .lightIsOff
+                        redLightCondition = .lightIsOn
+                        currentLight = .yellow
+                        
+                    case .yellow:
+                        redLightCondition = .lightIsOff
+                        yellowLightCondition = .lightIsOn
+                        currentLight = .green
+                    default:
+                        yellowLightCondition = .lightIsOff
+                        greenLightCondition = .lightIsOn
+                        currentLight = .red
+                    }
+                    
+                    if currentText == "START" {
+                        currentText = "NEXT"
+                    }
+                }) {
+                    Text(currentText)
+                        .font(.largeTitle)
+                        .bold()
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 60)
+                        .background(.blue)
+                        .cornerRadius(15)
+                        .overlay(RoundedRectangle(cornerRadius: 15)
+                            .stroke(Color.white, lineWidth: 3))
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
